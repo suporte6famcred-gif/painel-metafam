db84bb206ade434c7823b52cf0c207b6079dc9db
suporte6famcred-gif <suporte6famcred@gmail.com>
2026-09-08 11:59:14 -0300

    Create firebase.js

---
+++ meu painel/firebase.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDI3_Y3kdX-KCP14jYI3FX9NYawnRJIzpA",
+
+  authDomain: "meta-ativos.firebaseapp.com",
+
+  projectId: "meta-ativos",
+
+  storageBucket: "meta-ativos.firebasestorage.app",
+
+  messagingSenderId: "247415934030",
+
+  appId: "1:247415934030:web:8141fba355a70f17f2071f",
+
+  measurementId: "G-1S01QTJZZT"
+
+};
